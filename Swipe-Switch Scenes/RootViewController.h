c7ba163bed6bf638f6260f1e8c30455bda94ac41
Christopher Whitman <Chris@Christopher-Whitmans-MacBook-Pro.local>
2011-03-22 21:04:06 -0700

    Initial Commit

---
+++ Swipe-Switch Scenes/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Swipe-Switch Scenes
+//
+//  Created by Christopher Whitman on 3/22/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
